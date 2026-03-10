8c562c7326f8c63455576c289142c44ec7044dbf
KVP-Git-Lovable <prajwal@kvpcorp.com>
2026-03-10 15:37:49 +0530

    IOS crash setting

---
+++ ios/App/App/public/assets/index-BYhCnUxp.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BEAhghh-.js","assets/index-DBcz8fBk.js","assets/index-BS06kbDm.css"])))=>i.map(i=>d[i]);
+import{r as i,_ as r}from"./index-DBcz8fBk.js";const _=i("Device",{web:()=>r(()=>import("./web-BEAhghh-.js"),__vite__mapDeps([0,1,2])).then(e=>new e.DeviceWeb)});export{_ as Device};
